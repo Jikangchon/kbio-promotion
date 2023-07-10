79a97db86460997d2eb98824ea109c092d885e1a
JK Chon <scltvkorea@gmail.com>
2023-07-10 17:43:59 +0900

    230710_38

--- kbio-introducing-page/main.js
+++ kbio-introducing-page/main.js
@@ -1,6 +1,6 @@
 const toggleBtn = document.querySelector('.navBar_toggleBtn');
 const menu = document.querySelector('.navBar_menu');
 
-toggleBtn.addEventListener(`click`, () => {
-    menu.classList.toggle(`active`)
-})
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active')
+});
