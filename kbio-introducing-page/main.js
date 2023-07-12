e5692c1aabdf6b4a9c62d7017038b070101a693c
Jikangchon <scltvkorea@gmail.com>
2023-07-12 17:16:48 +0900

    230712_42

--- kbio-introducing-page/main.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector('.navBar_toggleBtn');
-const menu = document.querySelector('.navBar_menu');
-
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active')
-})
